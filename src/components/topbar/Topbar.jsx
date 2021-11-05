bc0580aff74357b010c6f1640359270786e7b5b6
Akash <akashverma21042001@gmail.com>
2021-11-05 19:51:29 +0530

    commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            React
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+8368515125</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>akashverma21042001@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
