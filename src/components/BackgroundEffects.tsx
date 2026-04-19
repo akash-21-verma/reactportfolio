ba7df236a1699030a73495dbb7a4b919f234bb60
akash <akashverma21042001@gmail.com>
2026-04-19 11:50:37 +0530

    bgeffect

--- src/components/BackgroundEffects.tsx
+++ src/components/BackgroundEffects.tsx
@@ -2,7 +2,7 @@
 
 export default function BackgroundEffects() {
   return (
-      <div className="fixed left-5 top-1/3 md:left-20 md:top-2/3 -translate-y-1/2 z-10 pointer-events-none">
+      <div className="fixed left-5 top-1/2 md:left-20 md:top-2/3 -translate-y-1/2 z-10 pointer-events-none">
       <p className="-rotate-90 origin-left text-xs tracking-[0.3em] text-neutral-400">
         BUILT WITH <span className="text-primary text-lg"> PASSION </span>  AND   💜
         {/* ❤️ */}
